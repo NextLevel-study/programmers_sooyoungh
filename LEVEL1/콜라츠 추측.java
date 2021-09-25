39e874636072086af93d2509f7f84c6731c6d67a
sooyoungh <77563814+sooyoungh@users.noreply.github.com>
2021-09-25 14:30:54 +0900

    Create 콜라츠 추측.java

---
+++ LEVEL1/콜라츠 추측.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int solution(int x) {
+        //int 오버플로우 방지용 타입 변경!
+        long num = x;
+        int answer = 0;
+        
+        while (num != 1) {
+            answer += 1;
+            if (answer == 500) { return -1; }
+
+            if (num % 2 == 0) {
+                num = num/2;
+            } else {
+                num = num*3 + 1;
+            }
+        }
+        return answer;
+    }
+}
