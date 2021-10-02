cdcb009be0ffea901ce779966e39f2eab9cbf846
Sooyoung <sooyoung.h8@gmail.com>
2021-10-03 01:49:44 +0900

    Update 소수 찾기.java

--- LEVEL1/소수 찾기.java
+++ LEVEL1/소수 찾기.java
@@ -16,3 +16,31 @@ class Solution {
         return answer;
     }
 }
+
+// BETTER
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        int arr[] = new int[n+1];
+        arr[0] = 0; arr[1] = 0;
+        
+        // 초기화
+        for (int i = 2 ; i <= n; i++) { arr[i] = i; }
+        
+        // 소수 아닌것들은 0으로 만들자
+        for (int i = 2 ; i <= (int)Math.sqrt(n) ; i++) {
+            // 이미 소수가 아닌거면 패스
+            if(arr[i] == 0) continue;
+            // 소수의 배수(=소수가 아님)들은 다 0으로 만들자
+            for (int j = 2 * i ; j <= n; j+=i) {
+                arr[j] = 0;
+            }
+        }
+
+        for (int i = 0; i <= n ; i++ ) {
+            if (arr[i] != 0) answer++;
+        }
+
+        return answer;
+    }
+}
