7c84483feb832da1f0718ade6c010ed65f00c254
Sooyoung <sooyoung.h8@gmail.com>
2021-10-04 23:55:28 +0900

    Create 문자열 다루기 기본.java

---
+++ LEVEL1/문자열 다루기 기본.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean solution(String s) {
+        boolean answer = true;
+        int length = s.length();
+        
+        if( length == 4 || length == 6) {
+			for (int i = 0; i < length ; i++) {
+				char ch = s.charAt(i);
+				if(ch < '0' || ch > '9') // 수가 아니면
+					answer = false;
+			}
+		} 
+		return answer;
+        
+    }
+}
