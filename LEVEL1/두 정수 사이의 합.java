2dbcd71f51a1318e7f44439f3f69b493ce28fcfd
Sooyoung <sooyoung.h8@gmail.com>
2021-10-08 23:47:45 +0900

    Create 두 정수 사이의 합.java

---
+++ LEVEL1/두 정수 사이의 합.java
@@ -0,0 +1,22 @@
+class Solution {
+    public long solution(int a, int b) {
+        long answer = 0;
+        
+        if (a > b) {
+            int temp = a;
+            a = b;
+            b = temp;
+            
+        }
+        int cnt = b-a+1;
+        
+        if(cnt == 1 ) return (long) a;
+        
+        for ( int i = 0; i < cnt ; i++,a++ ){
+            answer += a;
+            System.out.println(a);
+        }
+        
+        return answer;
+    }
+}
